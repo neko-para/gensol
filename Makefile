d1cfd6b94a569c1b7d97956f4d5c3692ab128afc
neko-para <liao.hy@outlook.com>
2017-12-30 12:09:49 +0800

    Add subdir support

--- Makefile
+++ Makefile
@@ -1,12 +1,27 @@
 PREFIX=/usr/local
-INST_PREFIX=/usr/local
 default: gensol
-libgensol/src/gensol.cpp.o: $(shell $(CXX) -MM ./src/gensol.cpp -std=c++11 -I./include -DTEST=123 | tr '\n' ' ' | tr '\\' ' ' | perl -pe 's/.*://')
+libgensol/src/gensol.cpp.o: $(shell $(CXX) -MM ./src/src/gensol.cpp -std=c++11 -I./src/include | tr '\n' ' ' | tr '\\' ' ' | perl -pe 's/.*://')
 	@mkdir -p `dirname $@`
 	@echo "Compile $<"
-	@$(CXX) -c -o $@ $< -std=c++11 -I./include -DTEST=123 -g -Ofast -Wall -Wextra
-libgensol: .output/libgensol.a
-.output/libgensol.a: libgensol/src/gensol.cpp.o
+	@$(CXX) -c -o $@ $< -std=c++11 -I./src/include -g -Ofast -Wall -Wextra
+libgensol: .output/libgensol/libgensol.a
+.output/libgensol/libgensol.a: libgensol/src/gensol.cpp.o
+	@mkdir -p `dirname $@`
+	@echo "Link $@"
+	@$(AR) rc $@ libgensol/src/gensol.cpp.o
+install.libgensol: libgensol
+	@mkdir -p $(PREFIX)/include
+	@echo "Install ./src/include/gensol.h"
+	@install -m 0644 ./src/include/gensol.h $(PREFIX)/include/`basename ./src/include/gensol.h`
+	@mkdir -p $(PREFIX)/lib
+	@echo "Install .outputlibgensol/libgensol.a"
+	@install -m 0644 .outputlibgensol/libgensol.a $(PREFIX)/lib/`basename .outputlibgensol/libgensol.a`
+libgensol/src/gensol.cpp.o: $(shell $(CXX) -MM ./src/gensol.cpp -std=c++11 -I./include | tr '\n' ' ' | tr '\\' ' ' | perl -pe 's/.*://')
+	@mkdir -p `dirname $@`
+	@echo "Compile $<"
+	@$(CXX) -c -o $@ $< -std=c++11 -I./include -g -Ofast -Wall -Wextra
+libgensol: .output/libgensol/libgensol.a
+.output/libgensol/libgensol.a: libgensol/src/gensol.cpp.o
 	@mkdir -p `dirname $@`
 	@echo "Link $@"
 	@$(AR) rc $@ libgensol/src/gensol.cpp.o
@@ -15,28 +30,15 @@ install.libgensol: libgensol
 	@echo "Install ./include/gensol.h"
 	@install -m 0644 ./include/gensol.h $(PREFIX)/include/`basename ./include/gensol.h`
 	@mkdir -p $(PREFIX)/lib
-	@echo "Install .output/libgensol.a"
-	@install -m 0644 .output/libgensol.a $(PREFIX)/lib/`basename .output/libgensol.a`
-gensol/src/main.cpp.o: $(shell $(CXX) -MM ./src/main.cpp -std=c++11 -I./include | tr '\n' ' ' | tr '\\' ' ' | perl -pe 's/.*://')
-	@mkdir -p `dirname $@`
-	@echo "Compile $<"
-	@$(CXX) -c -o $@ $< -std=c++11 -I./include -g -Ofast -Wall -Wextra
-gensol: .output/gensol
-.output/gensol: libgensol gensol/src/main.cpp.o
-	@mkdir -p `dirname $@`
-	@echo "Link $@"
-	@$(CXX) -o $@ gensol/src/main.cpp.o -L.output -lgensol -g -Ofast
-install.gensol: gensol
-	@mkdir -p $(PREFIX)/bin
-	@echo "Install .output/gensol"
-	@install -m 0755 .output/gensol $(PREFIX)/bin/`basename .output/gensol`
+	@echo "Install .outputlibgensol/libgensol.a"
+	@install -m 0644 .outputlibgensol/libgensol.a $(PREFIX)/lib/`basename .outputlibgensol/libgensol.a`
 clean:
 	@echo "Remove objects"
-	@-rm -f libgensol/src/gensol.cpp.o gensol/src/main.cpp.o
+	@-rm -f libgensol/src/gensol.cpp.o libgensol/src/gensol.cpp.o
 	@echo "Remove outputs"
-	@-rm -f .output/libgensol.a .output/gensol
+	@-rm -f .output/libgensol/libgensol.a .output/libgensol/libgensol.a
 .PHONY: clean
-install: install.libgensol install.gensol
+install: install.libgensol install.libgensol
 .PHONY: install
 viewcompiler:
 	@echo "c compiler: $(CC)"
--- gensol.lua
+++ gensol.lua
@@ -6,7 +6,6 @@ local function div(str)
 	tab = tab or ""
 	return #tab, key, value
 end
-
 local function newnode(file, row, dpt, key, val)
 	local obj = {
 		key = key,
@@ -18,22 +17,18 @@ local function newnode(file, row, dpt, key, val)
 		if d ~= dpt + 1 then
 			break
 		end
-		-- print(d, k, v)
 		obj.sub[#obj.sub + 1], row = newnode(file, row + 1, d, k, v)
 	end
 	return obj, row
 end
-
-local multi
-
-local function createnode(node, prt)
+local function createnode(node, prt, multi)
 	local obj = {
 		__key = node.key,
 		__value = node.value
 	}
 	local nxt
 	for k, v in pairs(node.sub) do
-		nxt = createnode(v, obj)
+		nxt = createnode(v, obj, multi)
 		if not multi[v.key] then
 			obj[v.key] = nxt
 		elseif obj[v.key] then
@@ -46,8 +41,7 @@ local function createnode(node, prt)
 		__index = prt
 	})
 end
-
-function node_load(file, mt)
+local function node_load(file, mt)
 	local f = io.open(file, "r")
 	if not f then
 		return nil
@@ -58,30 +52,8 @@ function node_load(file, mt)
 	end
 	local n = newnode(d, 1, -1)
 	f:close()
-	multi = mt or {}
-	n = createnode(n)
-	multi = nil
+	n = createnode(n, nil, mt or {})
 	return n
-end
-src_dir = arg[2]
-node = node_load(arg[1], {
-	target = true,
-	link = true,
-	depend = true,
-	include = true,
-	define = true,
-	library = true,
-	rpath = true,
-	source = true,
-	warning = true,
-	inst_bin = true,
-	inst_inc = true,
-	inst_lib = true,
-	inst_shr = true
-})
-if not node then
-	print("Failed to open "..arg[1])
-	os.exit(1)
 end
 suffixs = {
 	excutable = "",
@@ -102,160 +74,189 @@ function getExt(str)
 	local _, _, suf = string.find(str, ".*%.(%w+)")
 	return suf
 end
-function expPath(str)
+function expPath(str, src, out)
 	if string.sub(str, 1, 1) == "@" then
-		return src_dir..string.sub(str, 2)
+		return src..string.sub(str, 2), string.sub(str, 2)
+	elseif string.sub(str, 1, 1) == "!" then
+		return ".output"..string.sub(str, 2), string.sub(str, 2)
 	else
-		return str
+		return str, str
 	end
 end
 Makefile = io.open("Makefile", "w")
 Makefile:write("PREFIX=/usr/local\n")
-for k, v in pairs(node.var or {}) do
-	if k ~= "__key" and k ~= "__value" then
-		Makefile:write(string.format("%s=%s\n", k, v.__value))
-	end
-end
 objs = ""
 outs = ""
 insts = ""
-Makefile:write("default:")
-for _, target in pairs(node.target) do
-	if target.default and target.default.__value == "true" then
-		Makefile:write(" "..target.__value)
+function main(src_dir, have_default)
+	node = node_load(src_dir.."solution", {
+		target = true,
+		link = true,
+		depend = true,
+		include = true,
+		define = true,
+		library = true,
+		rpath = true,
+		source = true,
+		warning = true,
+		inst_bin = true,
+		inst_inc = true,
+		inst_lib = true,
+		inst_shr = true,
+		subdir = true
+	})
+	if not node then
+		print("Failed to open "..src_dir.."solution")
+		os.exit(1)
 	end
-end
-Makefile:write("\n")
-for _, target in pairs(node.target) do
-	local target_name = target.__value
-	local objlst = ""
-	for _, source in ipairs(target.source or {}) do
-		source = source.__value
-		local obj = source..".o"
-		local src = source
-		if string.sub(source, 1, 1) == "@" then
-			obj = string.sub(source, 2)..".o"
-			src = src_dir..string.sub(source, 2)
+	for k, v in pairs(node.var or {}) do
+		if k ~= "__key" and k ~= "__value" then
+			Makefile:write(string.format("%s=%s\n", k, v.__value))
 		end
-		obj = target_name.."/"..obj
-		objlst = objlst.." "..obj
-		Makefile:write(string.format("%s: $(shell %s -MM %s", obj, expect_compiler[getExt(src)], src))
-		if target.std then
-			Makefile:write(" -std="..target.std.__value)
-		end
-		for _, dir in ipairs(target.include or {}) do
-			Makefile:write(" -I"..expPath(dir.__value))
-		end
-		for _, mcr in ipairs(target.define or {}) do
-			Makefile:write(" -D"..mcr.__value)
-		end
-		Makefile:write(" | tr '\\n' ' ' | tr '\\\\' ' ' | perl -pe 's/.*://')\n\t@mkdir -p `dirname $@`\n")
-		Makefile:write("\t@echo \"Compile $<\"\n")
-		Makefile:write(string.format("\t@%s -c -o $@ $<", expect_compiler[getExt(src)]))
-		if target.type.__value == "library" then
-			Makefile:write(" -fPIC")
-		end
-		if target.std then
-			Makefile:write(" -std="..target.std.__value)
-		end
-		for _, dir in ipairs(target.include or {}) do
-			Makefile:write(" -I"..expPath(dir.__value))
-		end
-		for _, mcr in ipairs(target.define or {}) do
-			Makefile:write(" -D"..mcr.__value)
-		end
-		if target.debug and target.debug.__value == "true" then
-			Makefile:write(" -g")
-		end
-		if target.optimize then
-			Makefile:write(" -O"..target.optimize.__value)
-		end
-		for _, warn in ipairs(target.warning or {}) do
-			Makefile:write(" -W"..warn.__value)
+	end
+	if (have_default) then
+		Makefile:write("default:")
+		for _, target in pairs(node.target) do
+			if target.default and target.default.__value == "true" then
+				Makefile:write(" "..target.__value)
+			end
 		end
 		Makefile:write("\n")
 	end
-	local outfile = target_name..suffixs[target.type.__value]
-	local output = ".output/"..outfile
-	outs = outs.." "..output
-	objs = objs..objlst
-	Makefile:write(string.format([[%s: %s
+	for _, dir in pairs(node.subdir or {}) do
+		local subdir = expPath(dir.__value, src_dir)
+		if (string.sub(subdir, -1) ~= "/") then
+			main(subdir.."/")
+		else
+			main(subdir)
+		end
+	end
+	for _, target in pairs(node.target) do
+		local target_name = target.__value
+		local objlst = ""
+		for _, source in ipairs(target.source or {}) do
+			local src, obj = expPath(source.__value, src_dir)
+			obj = target_name.."/"..obj..".o"
+			objlst = objlst.." "..obj
+			Makefile:write(string.format("%s: $(shell %s -MM %s", obj, expect_compiler[getExt(src)], src))
+			if target.std then
+				Makefile:write(" -std="..target.std.__value)
+			end
+			for _, dir in ipairs(target.include or {}) do
+				Makefile:write(" -I"..expPath(dir.__value, src_dir))
+			end
+			for _, mcr in ipairs(target.define or {}) do
+				Makefile:write(" -D"..mcr.__value)
+			end
+			Makefile:write(" | tr '\\n' ' ' | tr '\\\\' ' ' | perl -pe 's/.*://')\n\t@mkdir -p `dirname $@`\n")
+			Makefile:write("\t@echo \"Compile $<\"\n")
+			Makefile:write(string.format("\t@%s -c -o $@ $<", expect_compiler[getExt(src)]))
+			if target.type.__value == "library" then
+				Makefile:write(" -fPIC")
+			end
+			if target.std then
+				Makefile:write(" -std="..target.std.__value)
+			end
+			for _, dir in ipairs(target.include or {}) do
+				Makefile:write(" -I"..expPath(dir.__value, src_dir))
+			end
+			for _, mcr in ipairs(target.define or {}) do
+				Makefile:write(" -D"..mcr.__value)
+			end
+			if target.debug and target.debug.__value == "true" then
+				Makefile:write(" -g")
+			end
+			if target.optimize then
+				Makefile:write(" -O"..target.optimize.__value)
+			end
+			for _, warn in ipairs(target.warning or {}) do
+				Makefile:write(" -W"..warn.__value)
+			end
+			Makefile:write("\n")
+		end
+		local outfile = target_name..suffixs[target.type.__value]
+		local output = ".output/"..target_name.."/"..outfile
+		outs = outs.." "..output
+		objs = objs..objlst
+		Makefile:write(string.format([[%s: %s
 %s:]], target_name, output, output))
-	for _, depend_target in ipairs(target.depend or {}) do
-		Makefile:write(" "..depend_target.__value)
-	end
-	Makefile:write(objlst.."\n\t@mkdir -p `dirname $@`\n\t@echo \"Link $@\"\n")
-	if target.type.__value == "archive" then
-		Makefile:write("\t@$(AR) rc $@"..objlst)
-	else
-		Makefile:write("\t@"..compilers[target.lang.__value].." -o $@")
-		if target.type.__value == "library" then
-			Makefile:write(" -shared")
+		for _, depend_target in ipairs(target.depend or {}) do
+			Makefile:write(" "..depend_target.__value)
 		end
-		Makefile:write(objlst)
-		for _, dir in ipairs(target.library or {}) do
-			Makefile:write(" -L"..expPath(dir.__value))
+		Makefile:write(objlst.."\n\t@mkdir -p `dirname $@`\n\t@echo \"Link $@\"\n")
+		if target.type.__value == "archive" then
+			Makefile:write("\t@$(AR) rc $@"..objlst)
+		else
+			Makefile:write("\t@"..compilers[target.lang.__value].." -o $@")
+			if target.type.__value == "library" then
+				Makefile:write(" -shared")
+			end
+			Makefile:write(objlst)
+			for _, dir in ipairs(target.library or {}) do
+				Makefile:write(" -L"..expPath(dir.__value, src_dir))
+			end
+			for _, lib in ipairs(target.link or {}) do
+				Makefile:write(" -l"..lib.__value)
+			end
+			for _, path in ipairs(target.rpath or {}) do
+				Makefile:write(" -Wl,--rpath="..path.__value)
+			end
+			if target.debug and target.debug.__value == "true" then
+				Makefile:write(" -g")
+			end
+			if target.optimize then
+				Makefile:write(" -O"..target.optimize.__value)
+			end
 		end
-		for _, lib in ipairs(target.link or {}) do
-			Makefile:write(" -l"..lib.__value)
-		end
-		for _, path in ipairs(target.rpath or {}) do
-			Makefile:write(" -Wl,--rpath="..path.__value)
-		end
-		if target.debug and target.debug.__value == "true" then
-			Makefile:write(" -g")
-		end
-		if target.optimize then
-			Makefile:write(" -O"..target.optimize.__value)
-		end
-	end
-	Makefile:write("\n")
-	if target.install then
-		insts = insts.." install."..target_name
-		Makefile:write(string.format("install.%s: %s\n", target_name, target_name))
-		local inst = target.install
-		if inst.inst_bin then
-			Makefile:write("\t@mkdir -p $(PREFIX)/bin\n")
-			for _, file in ipairs(inst.inst_bin) do
-				local p = expPath(file.__value)
-				Makefile:write(string.format([[
+		Makefile:write("\n")
+		if target.install then
+			insts = insts.." install."..target_name
+			Makefile:write(string.format("install.%s: %s\n", target_name, target_name))
+			local inst = target.install
+			if inst.inst_bin then
+				Makefile:write("\t@mkdir -p $(PREFIX)/bin\n")
+				for _, file in ipairs(inst.inst_bin) do
+					local p = expPath(file.__value, src_dir)
+					Makefile:write(string.format([[
 	@echo "Install %s"
 	@install -m 0755 %s $(PREFIX)/bin/`basename %s`
 ]], p, p, p))
+				end
 			end
-		end
-		if inst.inst_inc then
-			Makefile:write("\t@mkdir -p $(PREFIX)/include\n")
-			for _, file in ipairs(inst.inst_inc) do
-				local p = expPath(file.__value)
-				Makefile:write(string.format([[
+			if inst.inst_inc then
+				Makefile:write("\t@mkdir -p $(PREFIX)/include\n")
+				for _, file in ipairs(inst.inst_inc) do
+					local p = expPath(file.__value, src_dir)
+					Makefile:write(string.format([[
 	@echo "Install %s"
 	@install -m 0644 %s $(PREFIX)/include/`basename %s`
 ]], p, p, p))
+				end
 			end
-		end
-		if inst.inst_lib then
-			Makefile:write("\t@mkdir -p $(PREFIX)/lib\n")
-			for _, file in ipairs(inst.inst_lib) do
-				local p = expPath(file.__value)
-				Makefile:write(string.format([[
+			if inst.inst_lib then
+				Makefile:write("\t@mkdir -p $(PREFIX)/lib\n")
+				for _, file in ipairs(inst.inst_lib) do
+					local p = expPath(file.__value, src_dir)
+					Makefile:write(string.format([[
 	@echo "Install %s"
 	@install -m 0644 %s $(PREFIX)/lib/`basename %s`
 ]], p, p, p))
+				end
 			end
-		end
-		if inst.inst_shr then
-			Makefile:write("\t@mkdir -p $(PREFIX)/share/"..target_name.."\n")
-			for _, file in ipairs(inst.inst_shr) do
-				local p = expPath(file.__value)
-				Makefile:write(string.format([[
+			if inst.inst_shr then
+				Makefile:write("\t@mkdir -p $(PREFIX)/share/"..target_name.."\n")
+				for _, file in ipairs(inst.inst_shr) do
+					local p = expPath(file.__value, src_dir)
+					Makefile:write(string.format([[
 	@echo "Install %s"
 	@install -m 0644 %s $(PREFIX)/share/%s/`basename %s`
 ]], p, p, target_name, p))
+				end
 			end
 		end
 	end
 end
+main(arg[1], ".output/")
 Makefile:write([[
 clean:
 	@echo "Remove objects"
